2cc528f75b425f5f6e66cf4ce7b3fa3dc6631571
LightQuantum <self@lightquantum.me>
2023-01-05 01:20:28 +0800

    feat: allow backend override

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
+import os
+
 import torch
 from loguru import logger
 
 
 def get_device():
+    if (env_device := os.environ.get("DEVICE")) is not None:
+        logger.info(f"Using device from environment: {env_device}")
+        return env_device
     if torch.backends.mps.is_available() and torch.backends.mps.is_built():
         logger.info("MPS backend detected, using MPS")
         return "mps"
-    elif torch.cuda.is_available():
+    if torch.cuda.is_available():
         logger.info("CUDA detected, using CUDA")
         return "cuda"
-    else:
-        logger.info("No GPU detected, using CPU")
-        return "cpu"
+    logger.info("No GPU detected, using CPU")
+    return "cpu"
 
 
 DEVICE = get_device()
